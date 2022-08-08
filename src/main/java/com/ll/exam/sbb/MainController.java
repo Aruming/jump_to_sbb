85ab436021e0d06b29eab48e682149945819ae1c
Aruming <0430kar@naver.com>
2022-08-08 14:25:55 +0900

    요청 관련 문제

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,8 +3,13 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class MainController {
+    private int increaseNo = -1;
+
     @RequestMapping("/sbb")
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담는다.
@@ -43,5 +48,49 @@ public class MainController {
                 <h1>입력된 나이 : %d</h1>
                 <h1>안녕하세요, GET 방식으로 오셨군요.</h1>
                 """.formatted(age);
+    }
+
+    @GetMapping("plus")
+    @ResponseBody
+    public String plus(@RequestParam(defaultValue = "0")int a, @RequestParam(defaultValue = "0")int b){
+        return """
+                <h1> %d + %d = %d </h1>                
+                """.formatted(a, b, a+b);
+    }
+
+    @GetMapping("minus")
+    @ResponseBody
+    public String minus(@RequestParam(defaultValue = "0")int a, @RequestParam(defaultValue = "0")int b){
+        return """
+                <h1> %d - %d = %d </h1>                
+                """.formatted(a, b, a-b);
+    }
+
+    @GetMapping("increase")
+    @ResponseBody
+    public int increase(){
+        increaseNo++;
+        return increaseNo;
+    }
+
+    @GetMapping("gugudan")
+    @ResponseBody
+    public String gugudan(@RequestParam(defaultValue = "0")int dan, @RequestParam(defaultValue = "0")int limit){
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(dan, i, dan*i))
+                .collect(Collectors.joining("<br>"));
+    }
+
+    @GetMapping("mbti")
+    @ResponseBody
+    public String mbti(@RequestParam(defaultValue = "") String name){
+        String result = switch (name){
+            case "홍길동", "이몽룡" -> "INFP";
+            case "홍길순" -> "ENFP";
+            case "임꺽정" -> "INFJ";
+            case "본인" -> "ISTJ";
+            default -> "모른다";
+        };
+        return result;
     }
 }
