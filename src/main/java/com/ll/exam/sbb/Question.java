ca0514d52a756a24af7a8afe14de9784320e2009
Aruming <0430kar@naver.com>
2022-08-12 14:32:27 +0900

    answer 를 저장하는 올바른 방법으로 변경

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -27,4 +27,9 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     private List<Answer> answerList = new ArrayList<>();
+
+    public void addAnswer(Answer answer) {
+        answer.setQuestion(this);
+        getAnswerList().add(answer);
+    }
 }
