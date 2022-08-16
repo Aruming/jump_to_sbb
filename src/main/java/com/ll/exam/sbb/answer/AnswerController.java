0b9eaae14f9678f3d1e015ada080bf57ace7a8e7
Aruming <0430kar@naver.com>
2022-08-16 13:50:42 +0900

    AnswerController 추가

---
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -0,0 +1,26 @@
+package com.ll.exam.sbb.answer;
+
+import com.ll.exam.sbb.question.Question;
+import com.ll.exam.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable int id, String content) {
+        Question question = this.questionService.getQuestion(id);
+
+
+
+        return "redirect:/question/detail/%d".formatted(id);
+    }
+}
